af06485f57c0e289494b6e419cc35865d86e05d4
kkla320 <accounts_kkla@icloud.com>
2021-02-19 21:19:00 +0100

    Made capsule methode for async use

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const core = require('@actions/core');
 const fs = require('fs').promises;
 const path = require("path");
 
-try {
-    const regex = new RegExp(core.getInput('regex'), core.getInput('flags'));
-    const replacement = core.getInput('replacement');
-    const dir = core.getInput('dir');
+const main = async () => {
+  const regex = new RegExp(core.getInput('regex'), core.getInput('flags'));
+  const replacement = core.getInput('replacement');
+  const dir = core.getInput('dir');
 
-    let contents = await fs.readdir(dir);
-    for (const content of contents) {
-      const oldPath = path.join(dir, content);
-      const isDirectory = await fs.stat(oldPath).isDirectory;
-      if (isDirectory) {
-        continue;
-      }
-
-      const newFileName = content.replace(regex, replacement);
-      console.log(`Attempting to rename ${oldPath} to ${newPath}`);
-      const newPath = path.join(dir, newFileName);
-      await fs.rename(oldPath, newPath)
+  let contents = await fs.readdir(dir);
+  for (const content of contents) {
+    const oldPath = path.join(dir, content);
+    const isDirectory = await fs.stat(oldPath).isDirectory;
+    if (isDirectory) {
+      continue;
     }
-  } catch (error) {
-    core.setFailed(error.message);
+
+    const newFileName = content.replace(regex, replacement);
+    console.log(`Attempting to rename ${oldPath} to ${newPath}`);
+    const newPath = path.join(dir, newFileName);
+    await fs.rename(oldPath, newPath)
   }
+}
+
+main()
+  .then(() => {
+    console.log("Finished renaming")
+  }).catch((error) => {
+    core.setFailed(error.message);
+  });
